86d5f08ce7b77f63c187ae1eacdbb45bc373fa05
usedcoding <ja14759@gmail.com>
2023-12-29 19:28:40 +0900

    sms

--- src/main/java/com/example/sports/member/UserRepository.java
+++ src/main/java/com/example/sports/member/UserRepository.java
@@ -8,4 +8,6 @@ import java.util.Optional;
 @Repository
 public interface UserRepository extends JpaRepository<Member, Long> {
     Optional<Member> findByUsername(String username);
+
+    Optional<Member> findByNameAndPhone(String username, String phoneNum);
 }
--- src/main/java/com/example/sports/member/UserService.java
+++ src/main/java/com/example/sports/member/UserService.java
@@ -2,9 +2,11 @@ package com.example.sports.member;
 
 import com.example.sports.DataNotFoundException;
 import lombok.RequiredArgsConstructor;
+import org.springframework.http.ResponseEntity;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
 
+import java.util.NoSuchElementException;
 import java.util.Optional;
 
 @Service
@@ -30,6 +32,25 @@ public class UserService {
         } else {
             throw new DataNotFoundException("member not found");
         }
-
     }
+
+//    public ResponseEntity<?> sendSmsToFindEmail(FindEmailRequestDto requestDto) {
+//        String name = requestDto.getName();
+//        //수신번호 형태에 맞춰 "-"을 ""로 변환
+//        String phoneNum = requestDto.getPhoneNum().replaceAll("-","");
+//
+//        Member foundUser = userRepository.findByNameAndPhone(name, phoneNum).orElseThrow(()->
+//                new NoSuchElementException("회원이 존재하지 않습니다."));
+//
+//        String receiverEmail = foundUser.getUsername();
+//        String verificationCode = validationUtil.createCode();
+//        smsUtil.sendOne(phoneNum, verificationCode);
+//
+//        //인증코드 유효기간 5분 설정
+//        redisUtil.setDataExpire(verificationCode, receiverEmail, 60 * 5L);
+//
+//        return ResponseEntity.ok(new Message("SMS 전송 성공"));
+//    }
+
+
 }
